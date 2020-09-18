7d3d16a476e89fd581c6b9ee4a830ebe49ec760c
Yevhen Mosiichuk <zhenya9801@gmail.com>
2020-09-18 23:30:17 +0300

    Added Zuul api-gateway

---
+++ api-gateway/src/test/java/com/cinemabooking/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemabooking.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
